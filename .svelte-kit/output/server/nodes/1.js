3ecf7d5052628770a4b09e7661372aca4cdd8400
TommasoPrinetti <Tommi.prinetti@gmail.com>
2025-01-22 15:11:59 +0100

    Built with updated og_images permalink

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D5teYdN1.js","_app/immutable/chunks/disclose-version.yRUqBdr1.js","_app/immutable/chunks/runtime.CPZqnB3O.js","_app/immutable/chunks/legacy.BNj3VDte.js","_app/immutable/chunks/render.ClV9nQy9.js","_app/immutable/chunks/entry.CH0WLzM2.js","_app/immutable/chunks/index-client.CgKQl7V3.js"];
+export const imports = ["_app/immutable/nodes/1.ttNL2p10.js","_app/immutable/chunks/disclose-version.yRUqBdr1.js","_app/immutable/chunks/runtime.CPZqnB3O.js","_app/immutable/chunks/legacy.BNj3VDte.js","_app/immutable/chunks/render.ClV9nQy9.js","_app/immutable/chunks/entry.4z5-Krkr.js","_app/immutable/chunks/index-client.CgKQl7V3.js"];
 export const stylesheets = [];
 export const fonts = [];
