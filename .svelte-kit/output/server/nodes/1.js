9bccbb2d17cf64d2e2e0910877cbf43866d57169
Tommaso Prinetti <Tommi.prinetti@gmail.com>
2025-04-16 15:26:39 +0200

    Removed more useless files

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.4BoTNm2X.js","_app/immutable/chunks/CkJT2ZpQ.js","_app/immutable/chunks/CfUOohTe.js","_app/immutable/chunks/CJz518lz.js","_app/immutable/chunks/o3RMZ0sa.js","_app/immutable/chunks/DNqrX47K.js","_app/immutable/chunks/DVokPMIk.js","_app/immutable/chunks/CJ3UEpp_.js"];
-export const stylesheets = [];
-export const fonts = [];
