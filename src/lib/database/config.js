eb3a96bb745ea721c8884bd7d0131aa7510ed13e
TommasoPrinetti <Tommi.prinetti@gmail.com>
2025-04-24 09:56:16 +0200

    Updates regarding the content of the website

--- src/lib/database/config.js
+++ src/lib/database/config.js
@@ -17,12 +17,14 @@ import Ex_14 from "$media/photos/Ex_14.jpg?enhanced";
 import Ex_15 from "$media/photos/Ex_15.jpg?enhanced";
 import Ex_16 from "$media/photos/Ex_16.jpg?enhanced";
 import Ex_17 from "$media/photos/Ex_17.jpg?enhanced";
+import Ex_18 from "$media/photos/Ex_18.jpg?enhanced";
 
 //Capitols Covers
 import coverQualifying from "$media/capitols_cover/Cover_Qualifying.jpg?enhanced";
 import coverBenchmarking from "$media/capitols_cover/Cover_Benchmarking.jpg?enhanced";
 import coverPrompting from "$media/capitols_cover/Cover_Prompting.jpg?enhanced";
 import coverExcel from "$media/capitols_cover/Cover_Excel.jpg?enhanced";
+import coverDistilling from "$media/capitols_cover/Cover_Distilling.jpg?enhanced";
 
 //Logos and generic
 import enhanced_logoImage from "$media/logos/enhanced_logoImage.png?enhanced";
@@ -34,28 +36,28 @@ import googleSupport from "$media/logos/google_support.png?enhanced";
 
 //Floaters_content
 
-import Qualifying_floater_1 from "$lib/media/floaters/qualifying/Floater_qualifying_1.webm"
-import Qualifying_floater_2 from "$lib/media/floaters/qualifying/Floater_qualifying_2.webm"
-import Qualifying_floater_3 from "$lib/media/floaters/qualifying/Floater_qualifying_3.jpg?enhanced"
-import Qualifying_floater_4 from "$lib/media/floaters/qualifying/Floater_qualifying_4.jpg?enhanced"
+import Qualifying_floater_1 from "$lib/media/floaters/qualifying/Floater_qualifying_1.webm";
+import Qualifying_floater_2 from "$lib/media/floaters/qualifying/Floater_qualifying_2.webm";
+import Qualifying_floater_3 from "$lib/media/floaters/qualifying/Floater_qualifying_3.jpg?enhanced";
+import Qualifying_floater_4 from "$lib/media/floaters/qualifying/Floater_qualifying_4.jpg?enhanced";
 
 // Prompting floaters
-import Prompting_floater_1 from "$lib/media/floaters/prompting/Floater_prompting_1.webm"
-import Prompting_floater_2 from "$lib/media/floaters/prompting/Floater_prompting_2.webm"
-import Prompting_floater_3 from "$lib/media/floaters/prompting/Floater_prompting_3.jpg?enhanced"
-import Prompting_floater_4 from "$lib/media/floaters/prompting/Floater_prompting_4.jpg?enhanced"
+import Prompting_floater_1 from "$lib/media/floaters/prompting/Floater_prompting_1.webm";
+import Prompting_floater_2 from "$lib/media/floaters/prompting/Floater_prompting_2.webm";
+import Prompting_floater_3 from "$lib/media/floaters/prompting/Floater_prompting_3.jpg?enhanced";
+import Prompting_floater_4 from "$lib/media/floaters/prompting/Floater_prompting_4.jpg?enhanced";
 
 // Excelling floaters
-import Excelling_floater_1 from "$lib/media/floaters/excelling/Floater_excelling_1.jpg?enhanced"
-import Excelling_floater_2 from "$lib/media/floaters/excelling/Floater_excelling_2.webm"
-import Excelling_floater_3 from "$lib/media/floaters/excelling/Floater_excelling_3.webm"
-import Excelling_floater_4 from "$lib/media/floaters/excelling/Floater_excelling_4.webm"
+import Excelling_floater_1 from "$lib/media/floaters/excelling/Floater_excelling_1.jpg?enhanced";
+import Excelling_floater_2 from "$lib/media/floaters/excelling/Floater_excelling_2.webm";
+import Excelling_floater_3 from "$lib/media/floaters/excelling/Floater_excelling_3.webm";
+import Excelling_floater_4 from "$lib/media/floaters/excelling/Floater_excelling_4.webm";
 
 // Benchmarking floaters
-import Benchmarking_floater_1 from "$lib/media/floaters/benchmarking/Floater_benchmarking_1.webm"
-import Benchmarking_floater_2 from "$lib/media/floaters/benchmarking/Floater_benchmarking_2.webm"
-import Benchmarking_floater_3 from "$lib/media/floaters/benchmarking/Floater_benchmarking_3.jpg?enhanced"
-import Benchmarking_floater_4 from "$lib/media/floaters/benchmarking/Floater_benchmarking_4.jpg?enhanced"
+import Benchmarking_floater_1 from "$lib/media/floaters/benchmarking/Floater_benchmarking_1.webm";
+import Benchmarking_floater_2 from "$lib/media/floaters/benchmarking/Floater_benchmarking_2.webm";
+import Benchmarking_floater_3 from "$lib/media/floaters/benchmarking/Floater_benchmarking_3.jpg?enhanced";
+import Benchmarking_floater_4 from "$lib/media/floaters/benchmarking/Floater_benchmarking_4.jpg?enhanced";
 
 // Grouping the assets into objects for easy access
 export const photos = {
@@ -70,10 +72,13 @@ export const photos = {
   Ex_9,
   Ex_10,
   Ex_11,
+  Ex_12,
   Ex_13,
   Ex_14,
   Ex_15,
   Ex_16,
+  Ex_17,
+  Ex_18,
 };
 
 export const capitolsCover = {
@@ -81,6 +86,7 @@ export const capitolsCover = {
   coverBenchmarking,
   coverPrompting,
   coverExcel,
+  coverDistilling,
 };
 
 export const floaters = {
--- src/lib/database/global_db.js
+++ src/lib/database/global_db.js
@@ -128,7 +128,7 @@ const cardsDb = {
     IndexNum: 4,
     CoverImg: capitolsCover.coverExcel,
     Content: [
-       {
+      {
         title: "Exercise 12 - Choosing Your exemplary Work",
         subtitle:
           "Participants identify and reflect on written texts that set a benchmark in their field—models worth emulating.",
@@ -175,19 +175,20 @@ const cardsDb = {
     isProjCover: false,
   },
   Card5: {
-    Title: "NEW CARD",
+    Title: "Distilling",
+    Question:
+      "What story emerges when we look back on our evolving relationship with LLMs?",
     Description:
-      "In this block, participants use their chosen LLM to produce the highest quality work possible, attempting to recreate a masterpiece from their field. The goal is to push the model's capabilities to the fullest, testing its ability to reach the gold standard in a specialized field.",
-    Question: "How closely can an LLM approach human excellence?",
+      "In this final block, participants circle back to the essential aspects of their use of LLMs. Delving into previous exercises, they uncover key features that have remained stable or which have evolved during the course of the experiment, thus producing a detailed overview of the integration of LLMs within their workflow.",
     IndexNum: 5,
-    CoverImg: capitolsCover.coverExcel,
+    CoverImg: capitolsCover.coverDistilling,
     Content: [
-       {
-        title: "Exercise 12 - Choosing Your exemplary Work",
+      {
+        title: "Exercise 18 - Choosing Your exemplary Work",
         subtitle:
-          "Participants identify and reflect on written texts that set a benchmark in their field—models worth emulating.",
-        text: "An exemplary work is not necessarily a prestigious publication but also <b>modest, everyday writings</b> that embody clarity, precision, or impact within a professional context. By selecting both renowned and less conspicuous examples, the goal is to <b>sharpen awareness of what good writing looks like</b> across different formats and purposes.",
-        picture: photos.Ex_12,
+          "Participants synthesize their own perspective on the entire experiment into a cohesive narrative.",
+        text: "Rather than summarizing past exercises, this exercise encourages <b>the creation of a personal booklet</b> that combines selected pages from the vademecum with additional notes, sketches, and digital traces. This curated archive serves as both <b>a tangible memory and a storytelling device</b> —capturing key moments, doubts, and insights that have shaped the participant’s evolving relationship with LLMs.",
+        picture: photos.Ex_18,
       },
     ],
     bgColor: "#FFCE93",
@@ -236,13 +237,20 @@ const alterEgosDb = {
     bgColor: "#000000",
   },
   Card5: {
-    Title: "Context",
-    CoverImg: general.logoImage_white,
-    Question:
-      "How to document practices shaped and influenced by the project itself?",
+    Title: "Contact",
+    Question: "Our Team",
     Description:
-      "The booming rise of <b>large language models (LLMs)</b> such as ChatGPT has sparked a rush to produce discourse about these technologies. The quick crystallisation of a shared outlook around a few key themes has <b>narrowed the scope</b> of potential interrogations. Public and scientific debates focus on technical issues: algorithmic bias, confabulation, and intellectual property violations. <br><br>However, the problems and consequences associated with their <b>actual use</b> – for both their users and their professional contexts – remain largely unexplored. This asymmetry fuels a <b>mechanical view</b> of technological development and its effects, as if the technical analysis of these systems were enough to predict their social impact. <br><br>Moreover, these discourses present AI as a <b>monolithic and disruptive</b> entity, dismissing the possibility that it may be aligned with existing practices and that its effects may vary depending on situations encountered in one's job.<br><br>There is thus an <b>urgent need</b> to move beyond predictions about the future of work, to account for the professional contexts in which LLMs are used, and to identify <b>current issues</b> – not prospective ones. <br><br>How do AI's well-known problems (<b>bias, confabulation, etc.</b>) manifest in established practices? What new, unexpected problems are surfacing? How do LLMs shape individual work practices? And in turn, how do professional environments shape LLMs and their use?",
+      '[Donato Ricci](https://medialab.sciencespo.fr/equipe/donato-ricci/): Principal Investigator, Designer <br> <a href="mailto:donato.ricci@sciencespo.fr">Send Email</a><br><br>[Gabriel Alcaras](https://medialab.sciencespo.fr/equipe/gabriel-alcaras/): Postdoctoral researcher, Sociologist <br> <a href="mailto:gabriel.alcaras@sciencespo.fr">Send Email</a><br><br>[Tommaso Prinetti](https://medialab.sciencespo.fr/equipe/tommaso-prinetti/): Research Assistant, Designer <br> <a href="mailto:tommaso.prinetti@sciencespo.fr">Send Email</a><br><br>[Zoé de Vries](https://medialab.sciencespo.fr/equipe/zoe-de-vries/): Research Assistant, Linguist <br> <a href="mailto:zoe.devries@sciencespo.fr">Send Email</a><br><br>',
     IndexNum: 5,
+    Content: [
+      {
+        title: "Exercise 12 - Choosing Your exemplary Work",
+        subtitle:
+          "Participants identify and reflect on written texts that set a benchmark in their field—models worth emulating.",
+        text: "An exemplary work is not necessarily a prestigious publication but also <b>modest, everyday writings</b> that embody clarity, precision, or impact within a professional context. By selecting both renowned and less conspicuous examples, the goal is to <b>sharpen awareness of what good writing looks like</b> across different formats and purposes.",
+        picture: photos.Ex_12,
+      },
+    ],
     bgColor: "#000000",
   },
 };
@@ -375,5 +383,5 @@ const floatersDb = {
     media: floaters.Benchmarking_floater_4,
     href: "",
     parent: "Benchmarking",
-  }
+  },
 };
