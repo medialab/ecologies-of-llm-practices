16514e1d4049cc94d49745d85f59e1ca90b49444
TommasoPrinetti <Tommi.prinetti@gmail.com>
2025-06-20 16:01:50 +0200

    Oops I should learn sveltekit bind:this properties

--- src/lib/components/buttons/sharer.svelte
+++ src/lib/components/buttons/sharer.svelte
@@ -4,7 +4,7 @@ import { fade, scale, fly } from 'svelte/transition';
 import { cubicOut } from 'svelte/easing';
 import { writable } from 'svelte/store';
 
-let copierAlert = null;
+let copierAlert;
 
 // Utility function to truncate text with ellipsis
 const truncateText = (text, maxLength = 200) => {
@@ -139,21 +139,18 @@ const shareContent = () => {
 }
 
 const copyToClipboard = () => {
-
+    console.log("Copied link!")
     try {
         const linkToCopy = $shareInfo.url || `https://ecologiesofllm.medialab.sciencespo.fr/${$currentHash}`;
         navigator.clipboard.writeText(linkToCopy);
     } catch (error) {
         console.error('Failed to copy to clipboard:', error);
     }
+    copierAlert.classList.add('show');
 
-    if (copierAlert) {
-        copierAlert.classList.add('show');
-
-        setTimeout(() => {
-            copierAlert.classList.remove('show');
-        }, 1000);
-    }
+    setTimeout(() => {
+        copierAlert.classList.remove('show');
+    }, 1000);
 
 }
 
@@ -252,16 +249,19 @@ const shareOnReddit = () => {
         <div class="sharer_link_container">
             <div class="sharer_link_text">
                     <p class="p3">
-                        ecologiesofllm.medialab.sciencespo.fr/{$currentHash}
+                       {'ecologiesofllm.medialab.sciencespo.fr/' + $currentHash}
                     </p>
             </div>
             <button class="sharer_copier"
             style="background-color: {$currentCardColor};"
             onclick={copyToClipboard}
+            onkeydown={copyToClipboard}
+            onpointerdown={copyToClipboard}
+            ontouchstart={copyToClipboard}
             aria-label="Copy link to clipboard">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M362.31-260Q332-260 311-281q-21-21-21-51.31v-455.38Q290-818 311-839q21-21 51.31-21h335.38Q728-860 749-839q21 21 21 51.31v455.38Q770-302 749-281q-21 21-51.31 21H362.31Zm0-60h335.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-455.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H362.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v455.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85Zm-140 200Q192-120 171-141q-21-21-21-51.31v-515.38h60v515.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h395.38v60H222.31ZM350-320v-480 480Z"/></svg>
             <div class="copier_alert"
-            bindthis={copierAlert}
+            bind:this={copierAlert}
             style="background-color: {$currentCardColor};">
                 <p class="p3">Link copied!</p>
             </div>
